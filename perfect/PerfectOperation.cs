ede52c1e0d93bac002c3d3ac5b4e817864e15b6d
aditya <vinchuaditya@gmail.com>
2022-02-01 08:31:03 +0530

    perfect number

---
+++ perfect/PerfectOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace perfect
+{
+    internal class PerfectOperation
+    {
+
+        int i, sum;
+        int num;
+        public void Perfect()
+        {
+            Console.Write("Enter number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+            sum = 0;
+            while (i < num)
+            {
+                if (num % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == num)
+                Console.Write("{0} is Perfect Number", num);
+            else
+                Console.WriteLine("this is NOT perfect number ");
+
+            Console.Write("\n");
+        }
+    }
+}
---
+++ perfect/Program.cs
@@ -0,0 +1,16 @@
+﻿//6,28,496
+
+using System;
+
+class Perfect
+{
+ internal class PerfectOperation
+{   public static void Main()
+    {
+        PerfectOperation op = new PerfectOperation();
+        op.Perfect();
+    }
+}
+    
+}
+
